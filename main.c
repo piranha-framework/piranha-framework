4f8598910b37322c670e9134b393bf22c14c02db
Astra <unroot@localhost.localdomain>
2020-12-25 17:17:37 -0900

    Changing the environment file type

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <curl/curl.h>
 #include <stdlib.h>
+#include "config.h" // This header file containing the info about configuration of the framework
 
 int main(void) {
 	return 0;
