c9c31eb91781fbaf149e10fe6834eb0d5bd039e7
Masud karim <unroot@localhost.localdomain>
2020-12-30 10:49:32 -0900

    Http filter post, put, delete http method

--- server-container/http.c
+++ server-container/http.c
@@ -13,5 +13,32 @@ int http_get(char* buffer) {
 
 
 	if (result != NULL) return HTTP_GET;
-	else HTTP_ERROR;
+	else return HTTP_ERROR;
+}
+
+int http_post(char* buffer) {
+	char ret,result;
+
+	ret = strupr(buffer);
+	result = strstr(buffer, "POST");
+	if (result != NULL) return HTTP_POST;
+	else return HTTP_ERROR;
+}
+
+int http_put(char* buffer) {
+	char* ret, result;
+
+	ret = strupr(buffer);
+	result = strstr(buffer, "PUT");
+	if (result != NULL) return HTTP_PUT;
+	else return HTTP_ERROR;
+}
+
+int http_delete(char* buffer) {
+	char* ret, result;
+
+	ret = strupr(buffer);
+	result = strstr(buffer, "DELETE");
+	if (result != NULL) return HTTP_DELETE;
+	else return HTTP_ERROR;
 }
--- server-container/http.h
+++ server-container/http.h
@@ -3,6 +3,8 @@
 #define HTTP_GET 100
 #define HTT_POST 200
 #define HTTP_ERROR -100
+#define HTTP_PUT 300
+#define HTTP_DELETE 400
 
 int http_get(char *buffer);
 void http_post();
