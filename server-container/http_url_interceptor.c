4ada88cbcfca9f5c4eec44a8b967876de2699df8
Masud karim <msmasud578@gmail.com>
2021-05-25 10:25:49 -0800

    Adding http url interceptor

---
+++ server-container/http_url_interceptor.c
@@ -0,0 +1,5 @@
+#include "http_url_interceptor.h"
+
+int intercept_url_path(char* request_header) {
+
+}
---
+++ server-container/http_url_interceptor.h
@@ -0,0 +1,3 @@
+#pragma once
+
+int intercept_url_path(char* request_header); // this will extract the url path from the request header
